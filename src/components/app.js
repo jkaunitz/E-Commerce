6a4a35dd80b2fa7986e07558fbabf60da7cad854
jkaunitz <justin.kaunitz@gmail.com>
2020-09-09 13:49:43 -0700

    updating

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
+        <h1>Ecom</h1>
         <h2>React Redux Router</h2>
       </div>
     );
